a5a0957ad89a2294587b23882c4be4936f4215fe
Ethan Goldblum <egoldblum@linkedin.com>
2013-08-16 14:27:12 -0700

    Cleanup

--- scrape.js
+++ scrape.js
@@ -1,3 +1,5 @@
+const fs = require('fs');
+
 var request = require('request'),
 	jsdom = require('jsdom'),
 	opts = {
@@ -5,7 +7,7 @@ var request = require('request'),
 	},
 	JQUERY_URL = 'http://code.jquery.com/jquery-1.7.2.min.js',
 	PARSE_MAP = [
-		function (td) {
+		function (td, $) {
 			var name = $.trim($(td).text()),
 				a = $('a', td),
 				mountaineer = false,
@@ -22,7 +24,7 @@ var request = require('request'),
 
 			if (name.substr(-2) === '**') {
 				mountaineer = false;
-				emblem = true
+				emblem = true;
 			}
 
 
@@ -31,56 +33,57 @@ var request = require('request'),
 				sp_url: url,
 				mountaineer: mountaineer,
 				emblem: emblem
-			}
+			};
 		},
-		function (td) {
+		function (td, $) {
 			return {
 				elevation: +$(td).text().replace(',', '')
-			}
+			};
 		},
-		function (td) {
+		function (td, $) {
 			return {
 				lat: $.trim($(td).text())
-			}
+			};
 		},
-		function (td) {
+		function (td, $) {
 			return {
 				lon: $.trim($(td).text())
-			}
+			};
 		},
-		function (td) {
+		function (td, $) {
 			return {
 				region: $.trim($(td).text())
-			}
+			};
 		}
 	];
 
 request(opts, function (err, resp, body) {
 	if (err) {
 		console.log('Error fetching page ' + opts.uri, err);
-		exit(1);
+		process.exit(1);
 	}
 
 	jsdom.env({
 		html: body,
-		scripts: [JQUERY_URL]
-	}, function (err, window) {
-		$ = window.jQuery;
-		var trs = $('.list table tr:not(:first-child)'),
-			data;
+		scripts: [JQUERY_URL],
+    done: function (err, window) {
+      var $ = window.jQuery;
+      var trs = $('.list table tr:not(:first-child)'),
+        data;
 
-			data = $.map(trs, function (tr, i) {
-				var tds = $('td', tr),
-					peak = {};
+      data = $.map(trs, function (tr) {
+        var tds = $('td', tr),
+            peak = {};
 
-				$.each(tds, function (j, td) {
-					var val = PARSE_MAP[j](td);
-					$.extend(peak, val);
-				});
+        $.each(tds, function (j, td) {
+            var val = PARSE_MAP[j](td, $);
+            $.extend(peak, val);
+          });
 
-				return peak;
-			});
+        return peak;
+      });
 
-			console.log(data);
-	});
+      fs.writeFileSync('./peaks.json', JSON.stringify(data));
+    }
+  });
 });
